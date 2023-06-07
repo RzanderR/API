76fbb9bb78e55832eba0ed330edc4f9da1d274fd
zzz <zander.rothstein@gmail.com>
2023-06-06 17:25:31 -0700

    Create weather api

---
+++ weather.py
@@ -0,0 +1,24 @@
+import requests
+
+apiKey = "3091a9532ae0a303182c0a908bc671bc"
+city = input("What city? : ")
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}"
+
+response = requests.get(url)
+data = response.json()
+
+try:
+  data["main"]
+except:
+  print("City doesn't exist")
+  exit()
+
+temp = data["main"]["temp"]
+temp = (temp - 273.15) * (9 / 5) + 32
+weather = data['weather'][0]['description']
+humidity = data['main']['humidity']
+
+print(f"\nTemperature: {round(temp, 1)}%")
+print(f"Weather: {weather}")
+print(f"Humidity: {humidity}%")
